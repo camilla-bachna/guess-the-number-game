e6ffff3bb2bf98c375472dc78b8b8f2778bcf5e7
camilla-bachna <camilla.bachna@gmail.com>
2021-01-28 10:52:49 +0100

    add preventDefault, move random number outside of function and value of input inside function

--- js/main.js
+++ js/main.js
@@ -2,27 +2,30 @@
 
 const numberElement = document.querySelector(".js-number");
 const buttonElement = document.querySelector(".js-button");
+const hintElement = document.querySelector(".js-hint");
 
-const numberValue = () => {
-  let value = parseInt(numberElement.innerHTML);
-  console.log(value);
-};
-
-function buttonHandler() {
-
-    //get random number
-    const randomNumer = Math.ceil(Math.random() * 100);
+const randomNumer = Math.ceil(Math.random() * 100);
 console.log(randomNumer);
 
+function buttonHandler(ev) {
+    ev.preventDefault()
+    let valueInput = parseInt(numberElement.value);
+    console.log(valueInput);
+    
+  //get random number and check with input value
+  if (randomNumer === valueInput) {
+    hintElement.innerHTML = "Has ganado campeona!!!";
+  } else if (randomNumer < valueInput) {
+    hintElement.innerHTML = "Demasiado alto.";
+  } else if (randomNumer > valueInput) {
+    hintElement.innerHTML = "Demasiado bajo.";
+  }
 
   //count intents
-  const numberIntentsElement = document.querySelector('.js-numberIntents');
+  const numberIntentsElement = document.querySelector(".js-numberIntents");
   let value = parseInt(numberIntentsElement.innerHTML);
-  value++; 
+  value++;
   numberIntentsElement.innerHTML = value;
 };
 
-
-buttonElement.addEventListener('click', buttonHandler);
-
-
+buttonElement.addEventListener("click", buttonHandler);
